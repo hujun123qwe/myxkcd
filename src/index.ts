72c168316aa9e91928ec42272b7d13d6c65466d3
hujun123qwe <hujun123qwe@163.com>
2019-06-05 20:46:15 +0800

    Add styling, attribution

--- src/index.ts
+++ src/index.ts
@@ -25,10 +25,29 @@ const extension: JupyterLabPlugin<void> = {
     console.log('ICommandPalette:', palette);
 
     let widget: Widget = new Widget();
+    let img = document.createElement('img');
+    widget.node.appendChild(img);
     widget.id = 'xkcd-jupyterlab';
     widget.title.label = 'xkcd.com';
     widget.title.closable = true;
+    widget.addClass('jp-xkcdWidget);
+    img.className = 'jp-xkcdCartoon';
 
+    img.insertAdjacentHTML('afterend',
+       `<div class="jp-xkcdAttribution">
+	    <a href="https://creativecommons.org/licenses/by-nc/2.5/" class="jp-xkcdAttribution" target="_blank">
+		    <img src="https://licensebuttons.net/l/by-nc/2.5/80x15.png" />
+	    </a>
+        </div>`
+    );
+
+    fetch('https:////egszlpbmle.execute-api.us-east-1.amazonaws.com/prod').then(response=>{
+      return response.json();
+    }).then(data=>{
+      img.src = data.img;
+      img.alt = data.title;
+      img.title = data.alt;
+    });
     const command: string = 'xkcd:open';
     app.commands.addCommand(command, {
       label: 'Random xkcd comic',
