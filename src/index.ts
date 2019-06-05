cdee827a533c66e0c3271d220433a033f64835da
root <root@hujun.localdomain>
2019-06-05 20:27:08 +0800

    Show xkcd command on pannel

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,12 @@
 import {
+  Widget
+} from '@phosphor/widgets';
+
+import {
+  ICommandPalette
+} from '@jupyterlab/apputils';
+
+import {
   JupyterLab, JupyterLabPlugin
 } from '@jupyterlab/application';
 
@@ -11,9 +19,30 @@ import '../style/index.css';
 const extension: JupyterLabPlugin<void> = {
   id: 'jupyterlab_xkcd',
   autoStart: true,
-  activate: (app: JupyterLab) => {
+  requires: [ICommandPalette],
+  activate: (app: JupyterLab, palette: ICommandPalette) => {
     console.log('JupyterLab extension jupyterlab_xkcd is activated!');
+    console.log('ICommandPalette:', palette);
+
+    let widget: Widget = new Widget();
+    widget.id = 'xkcd-jupyterlab';
+    widget.title.label = 'xkcd.com';
+    widget.title.closable = true;
+
+    const command: string = 'xkcd:open';
+    app.commands.addCommand(command, {
+      label: 'Random xkcd comic',
+      execute: ()=>{
+        if(!widget.isAttached){
+	  app.shell.addToMainArea(widget);
+	}
+	app.shell.activateById(widget.id);
+      }
+    });
+    palette.addItem({command, category:'Tutorial'});
   }
 };
 
 export default extension;
+
+
