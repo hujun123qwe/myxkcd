6aab55555331eb50327eb9eec734fcf642ac05cc
hujun123qwe <hujun123qwe@163.com>
2019-06-06 09:03:16 +0800

    Using VScode tools instand of vim

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 import {
+  Message
+} from '@phosphor/messaging';
+
+import {
   Widget
 } from '@phosphor/widgets';
 
@@ -13,7 +17,40 @@ import {
 import '../style/index.css';
 
 
-/**
+class XkcdWidget extends Widget{
+  constructor(){
+    super();
+    this.id = 'xkcd-jupyterlab';
+    this.title.label = 'xkcd.com';
+    this.title.closable = true;
+    this.addClass('jp-xkcdWidget');
+    this.img = document.createElement('img');
+    this.img.className = 'jp-xkcdCartoon';
+    this.node.appendChild(this.img);
+
+    this.img.insertAdjacentHTML('afterend',
+          `<div class="jp-xkcdAttribution">
+              <a href="https://creativecommons.org/licenses/by-nc/2.5/" class="jp-xkcdAttribution" target="_blank">
+              <img src="https://licensebuttons.net/l/by-nc/2.5/80x15.png" />
+            </a>
+          </div>`
+     );
+   }
+   readonly img: HTMLImageElement;
+
+   onUpdateRequest(msg: Message): void{
+     fetch('https://egszlpbmle.execute-api.us-east-1.amazonaws.com/prod').then(response => {
+       return response.json()
+     }).then(data => {
+       this.img.src = data.img;
+       this.img.alt = data.title;
+       this.img.title = data.alt;
+     });
+   }
+ };
+
+
+**
  * Initialization data for the jupyterlab_xkcd extension.
  */
 const extension: JupyterLabPlugin<void> = {
@@ -30,7 +67,7 @@ const extension: JupyterLabPlugin<void> = {
     widget.id = 'xkcd-jupyterlab';
     widget.title.label = 'xkcd.com';
     widget.title.closable = true;
-    widget.addClass('jp-xkcdWidget);
+    widget.addClass('jp-xkcdWidget');
     img.className = 'jp-xkcdCartoon';
 
     img.insertAdjacentHTML('afterend',
